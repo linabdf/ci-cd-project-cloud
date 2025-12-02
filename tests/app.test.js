8e8802b917146e8f4cdac3bb0ff61685750b802c
boudefoua <linaboudefoua12@gmail.com>
2025-12-02 20:53:23 +0100

    refaire les test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,13 +1,37 @@
-import { removeNumber } from "../utils.js";
+import {
+    toUpper,
+    reverseText,
+    wordCount,
+    charCount,
+    isPalindrome,
+    countVowels
+} from './utils.js';
 
-test("removeNumber enlève tous les chiffres", () => {
-    expect(removeNumber("abc123")).toBe("abc");
+test("toUpper should convert text to uppercase", () => {
+    expect(toUpper("hello")).toBe("HELLO");
 });
 
-test("removeNumber garde les lettres et caractères spéciaux", () => {
-    expect(removeNumber("Bonjour!42")).toBe("Bonjour!");
+test("reverseText should invert text", () => {
+    expect(reverseText("abc")).toBe("cba");
 });
 
-test("removeNumber avec texte sans chiffre", () => {
-    expect(removeNumber("Test")).toBe("Test");
+test("wordCount should count words correctly", () => {
+    expect(wordCount("hello world")).toBe(2);
+    expect(wordCount("   hello   world   ")).toBe(2);
+    expect(wordCount("")).toBe(0);
+});
+
+test("charCount should count characters without spaces", () => {
+    expect(charCount("hello world")).toBe(10);
+});
+
+test("isPalindrome should detect palindromes", () => {
+    expect(isPalindrome("kayak")).toBe(true);
+    expect(isPalindrome("A man a plan a canal Panama")).toBe(true);
+    expect(isPalindrome("hello")).toBe(false);
+});
+
+test("countVowels should count vowels including accentuated", () => {
+    expect(countVowels("bonjour")).toBe(3);      // o, o, u
+    expect(countVowels("Éléphant")).toBe(3);     // é, é, a
 });
